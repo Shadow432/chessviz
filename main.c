622bc19637138a1ca03a96d80259186320a24ea5
Ивченко Никита Сергеевич <ip715s08@mwp02.csc.local>
2018-02-20 14:26:23 +0700

    Black and White

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int main ()
 {
   printf ("\
- R K B K Q B K R\n\
- P P P P P P P P\n\
-    \n\
-    \n\
+ r k b k q b k r\n\
+ p p p p p p p p\n\
+                \n\
+                \n\
  P P P P P P P P\n\
  R K B K Q B K R");
 return 0;
